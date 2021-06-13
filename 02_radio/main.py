2a5f42018e933d911b5ea7d7442aeeeaeed4a8b3
daikosh <daikoshg@gmail.com>
2021-06-13 13:54:26 +0900

    minor change

--- 02_radio/main.py
+++ 02_radio/main.py
@@ -23,7 +23,7 @@ def open_radio_expander(title, imgpath, mp3path):
         #col1, col2, col3 = st.beta_columns(3)
         if os.path.exists(imgpath):
             image = Image.open(imgpath)
-            st.image(image, output_format="jpeg")#, width=700)
+            st.image(image, output_format="jpeg", width=700)
         if os.path.exists(mp3path):
             audio_file = open(mp3path, 'rb')
             audio_bytes = audio_file.read()
