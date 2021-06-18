773c98feeb34ec3531197dbc62bb607687ac5d22
daikosh <daikoshg@gmail.com>
2021-06-19 08:05:34 +0900

    minor change

--- 04_party1/main.py
+++ 04_party1/main.py
@@ -33,11 +33,11 @@ def main():
     ## Body ##
     with st.beta_expander("日時・会場", expanded=True):
         st.markdown('<p style="font-size: 28px">日時: 2022年3月26日 (土)', unsafe_allow_html=True)
-        st.markdown('<p style="font-size: 28px">会場: <a href="https://www.fortunegarden.com/?utm_source=googleMyBusiness&utm_medium=referral" target="_blank">Fortune Garden 京都</a></p>', unsafe_allow_html=True)
-        show_image("04_party1/01_fortune_garden.png", "png")
-        st.markdown(
-            '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3267.7758221667495!2d135.7665824152407!3d35.012316480355665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6001088d75e52813%3A0xfa711b5fcec57e9c!2z44OV44Kp44O844OB44Ol44Oz44Ks44O844OH44Oz5Lqs6YO9772cRk9SVFVORSBHQVJERU4gS1lPVE8!5e0!3m2!1sja!2sjp!4v1623846798297!5m2!1sja!2sjp" width="600" height="450" style="border:0;"allowfullscreen="" loading="lazy"></iframe>', unsafe_allow_html=True
-            )
+        st.markdown('<p style="font-size: 28px">会場: 未定 </p>', unsafe_allow_html=True)
+        #show_image("04_party1/01_fortune_garden.png", "png")
+        #st.markdown(
+        #    '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3267.7758221667495!2d135.7665824152407!3d35.012316480355665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6001088d75e52813%3A0xfa711b5fcec57e9c!2z44OV44Kp44O844OB44Ol44Oz44Ks44O844OH44Oz5Lqs6YO9772cRk9SVFVORSBHQVJERU4gS1lPVE8!5e0!3m2!1sja!2sjp!4v1623846798297!5m2!1sja!2sjp" width="600" height="450" style="border:0;"allowfullscreen="" loading="lazy"></iframe>', unsafe_allow_html=True
+        #    )
 
     with st.beta_expander("その他", expanded=True):
         pass
