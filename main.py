5d1e60797904b9e03f5bc569b7aac5b3b5bced1e
daikosh <daikoshg@gmail.com>
2021-06-19 08:10:37 +0900

    minor change

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def main():
     login_expander = st.beta_expander("ログインセクション / Login Section", expanded=True)
     username = login_expander.text_input("ユーザ名 / Username")
     password = login_expander.text_input("パスワード / Password", value="", type="password")
-    login_button = login_expander.button("ログイン / LOGIN")
+    #login_button = login_expander.button("ログイン / LOGIN")
 
     #login_expander.info("Authorized Personnel Only")
     login_expander.subheader("こちらは招待者専用のホームページです。URLやログイン情報は絶対に流出させないでください。")
