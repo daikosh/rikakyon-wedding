a105159d9519450266a3e510120f1e57183ee900
daikosh <daikoshg@gmail.com>
2021-06-19 09:09:23 +0900

    minor change

--- 03_profile/main.py
+++ 03_profile/main.py
@@ -32,19 +32,19 @@ def main():
 
     ## Body ##
     #show_image("03_profile/21_directors.png", "png")
-    with st.beta_expander("新郎_新婦", expanded=True):
+    with st.beta_expander("新郎・新婦", expanded=True):
     #st.header("取締役")
         col1, col2 = st.beta_columns(2)
         with col1:
             show_image("03_profile/01_kyosuke.png", "png")
             write_text("松井 響介", "24", "Black", "center")
             write_text("新郎", "16", "Black", "center")
-            write_text("", "14", "Black", "justify")
+            #write_text("", "14", "Black", "justify")
         with col2:
             show_image("03_profile/07_rikako.png", "png")
             write_text("犬飼 理香子", "24", "Black", "center")
             write_text("新婦", "16", "Black", "center")
-            write_text("", "14", "Black", "justify")
+            #write_text("", "14", "Black", "justify")
     #show_image("03_profile/22_executive.png", "png")
     with st.beta_expander("企画メンバー", expanded=True):
     #st.header("執行役員")
@@ -53,21 +53,21 @@ def main():
         with col1:
             show_image("03_profile/05_akuzawa.png", "png")
             write_text("阿久澤 拓也", "24", "Black", "center")
-            write_text("変態", "16", "Black", "center")
-            write_text("", "14", "Black", "justify")
+            #write_text("変態", "16", "Black", "center")
+            #write_text("", "14", "Black", "justify")
             show_image("03_profile/02_daikosh.png", "png")
             write_text("松岡 大起", "24", "Black", "center")
-            write_text("婚活", "16", "Black", "center")
-            write_text("", "14", "Black", "justify")
+            #write_text("婚活", "16", "Black", "center")
+            #write_text("", "14", "Black", "justify")
         with col2:
             show_image("03_profile/04_rinrin.png", "png")
             write_text("林 寛人", "24", "Black", "center")
-            write_text("IWSP", "16", "Black", "center")
-            write_text("コメント", "14", "Black", "justify")
+            #write_text("IWSP", "16", "Black", "center")
+            #write_text("", "14", "Black", "justify")
             show_image("03_profile/06_nobuaki.png", "png")
             write_text("吉種 伸彰", "24", "Black", "center")
-            write_text("教授", "16", "Black", "center")
-            write_text("", "14", "Black", "justify")
+            #write_text("教授", "16", "Black", "center")
+            #write_text("", "14", "Black", "justify")
 
 if __name__ == "__main__":
     main()
