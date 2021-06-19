2e487aefd244c0d1f31fa5de08c9675aa8c82782
daikosh <daikoshg@gmail.com>
2021-06-19 09:30:24 +0900

    minor change

--- 01_homepage/main.py
+++ 01_homepage/main.py
@@ -27,6 +27,7 @@ def main():
         image = Image.open(imgpath)
         st.image(image, output_format="jpeg", use_column_width="auto")
     st.write("どうも、皆さん、おはこんばんにちは。\n\n二次会に参加する人もしない人も楽しんで頂けるような様々なコンテンツを配信していきます。\n\n要望、リクエスト等があれば是非LINEへメッセージを送ってください。")
+    st.write("")
     st.write("松井響介、犬飼理香子")
     st.write("")
     st.write("プロジェクトメンバー")
