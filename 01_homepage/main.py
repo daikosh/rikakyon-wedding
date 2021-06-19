467b1804a23c5a403a3a6fece436757e1491c332
daikosh <daikoshg@gmail.com>
2021-06-19 09:36:25 +0900

    minor change

--- 01_homepage/main.py
+++ 01_homepage/main.py
@@ -30,7 +30,9 @@ def main():
     if os.path.exists(imgpath):
         image = Image.open(imgpath)
         st.image(image, output_format="jpeg", use_column_width="auto")
-    st.write("どうも、皆さん、おはこんばんにちは。\n\n二次会に参加する人もしない人も楽しんで頂けるような様々なコンテンツを配信していきます。\n\n要望、リクエスト等があれば是非LINEへメッセージを送ってください。")
+    st.write("結婚式二次会特設サイトにアクセスいただきありがとうございます。")
+    st.write("二次会に参加する人もしない人も楽しんで頂けるような様々なコンテンツを配信していきます。")
+    st.write("要望、リクエスト等があれば是非LINEへメッセージを送ってください。")
     st.write("")
     write_text("プロジェクトメンバー　一同", 16, "black", "right")
 if __name__ == "__main__":
