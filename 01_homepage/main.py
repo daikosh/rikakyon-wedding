af6e3d9dbc5c2e36fcb2712f8d6c4a269a3b8020
daikosh <daikoshg@gmail.com>
2021-06-19 15:28:34 +0900

    minor change

--- 01_homepage/main.py
+++ 01_homepage/main.py
@@ -36,6 +36,6 @@ def main():
     write_text("響介&理香子 結婚式二次会特設サイトにアクセスいただきありがとうございます。", 20, "black", "left")
     write_text("二次会に参加する人もしない人も楽しんで頂けるような様々なコンテンツを配信していきます。", 20, "black", "left")
     write_text("要望、リクエスト等があれば是非LINEへメッセージを送ってください。", 20, "black", "left")
-    write_text("EN-Jakee PJ メンバー　一同", 20, "black", "right")
+    write_text("運営メンバー　一同", 20, "black", "right")
 if __name__ == "__main__":
     main()
