b4ac288d71c35cdc48855b302eb865511f6c5fcf
daikosh <daikoshg@gmail.com>
2021-07-03 11:18:00 +0900

    profile

--- 03_profile/main.py
+++ 03_profile/main.py
@@ -21,57 +21,35 @@ def main(debug):
     initialization()
 
     ## Body ##
-    if debug is False:
-        with st.beta_expander("MAIN CAST", expanded=True):
-            col1, col2 = st.beta_columns(2)
-            with col1:
-                show_image("03_profile/01_kyosuke.png", "png")
-                write_text("Kyosuke Matsui", "24", "Black", "center")
-            with col2:
-                show_image("03_profile/07_rikako.png", "png")
-                write_text("Rikako Inukai", "24", "Black", "center")
-        with st.beta_expander("PROJECT MEMBER", expanded=True):
-            col1, col2 = st.beta_columns(2)
-            with col1:
-                show_image("03_profile/05_akuzawa.png", "png")
-                write_text("Takuya Akuzawa", "24", "Black", "center")
-                show_image("03_profile/02_daikosh2.png", "png")
-                write_text("daikosh", "24", "Black", "center")
-            with col2:
-                show_image("03_profile/04_rinrin.png", "png")
-                write_text("Hiroto Hayashi", "24", "Black", "center")
-                show_image("03_profile/06_nobuaki.png", "png")
-                write_text("Nobuaki Yoshitane", "24", "Black", "center")
-    else:
-        st.markdown("""
-            響介&理香子と愉快な仲間たちをご紹介。
-        """)
+    st.markdown("""
+        響介&理香子と愉快な仲間たちをご紹介。
+    """)
 
-        imgpath = "line.png"
-        if os.path.exists(imgpath):
-            image = Image.open(imgpath)
-            st.image(image, output_format="png", use_column_width="auto")
+    imgpath = "line.png"
+    if os.path.exists(imgpath):
+        image = Image.open(imgpath)
+        st.image(image, output_format="png", use_column_width="auto")
 
-        st.title("MAIN CAST")
-        show_image("03_profile/01_kyosuke.png", "png")
-        write_text("Kyosuke Matsui", "24", "Black", "center")
-        show_image("03_profile/07_rikako.png", "png")
-        write_text("Rikako Inukai", "24", "Black", "center")
+    st.title("MAIN CAST")
+    show_image("03_profile/01_kyosuke.png", "png")
+    write_text("Kyosuke Matsui", "24", "Black", "center")
+    show_image("03_profile/07_rikako.png", "png")
+    write_text("Rikako Inukai", "24", "Black", "center")
 
-        imgpath = "line.png"
-        if os.path.exists(imgpath):
-            image = Image.open(imgpath)
-            st.image(image, output_format="png", use_column_width="auto")
+    imgpath = "line.png"
+    if os.path.exists(imgpath):
+        image = Image.open(imgpath)
+        st.image(image, output_format="png", use_column_width="auto")
 
-        st.title("PROJECT MEMBER")
-        show_image("03_profile/05_akuzawa.png", "png")
-        write_text("Takuya Akuzawa", "24", "Black", "center")
-        show_image("03_profile/02_daikosh2.png", "png")
-        write_text("daikosh", "24", "Black", "center")
-        show_image("03_profile/04_rinrin.png", "png")
-        write_text("Hiroto Hayashi", "24", "Black", "center")
-        show_image("03_profile/06_nobuaki.png", "png")
-        write_text("Nobuaki Yoshitane", "24", "Black", "center")
+    st.title("PROJECT MEMBER")
+    show_image("03_profile/05_akuzawa.png", "png")
+    write_text("Takuya Akuzawa", "24", "Black", "center")
+    show_image("03_profile/02_daikosh2.png", "png")
+    write_text("daikosh", "24", "Black", "center")
+    show_image("03_profile/04_rinrin.png", "png")
+    write_text("Hiroto Hayashi", "24", "Black", "center")
+    show_image("03_profile/06_nobuaki.png", "png")
+    write_text("Nobuaki Yoshitane", "24", "Black", "center")
 
 if __name__ == "__main__":
     main(debug)
