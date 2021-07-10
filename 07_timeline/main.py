cb12bb9409b995f0d4d0a45ca14db084f57d9de7
daikosh <daikoshg@gmail.com>
2021-07-10 10:37:51 +0900

    timeline

--- 07_timeline/main.py
+++ 07_timeline/main.py
@@ -33,25 +33,28 @@ def main(debug):
     with st.beta_expander("第1章 幼少期", expanded=True):
         imgpath = "07_timeline/Timeline.png"
         show_image(imgpath, "png")
-    with st.beta_expander("第2章 中高校生", expanded=True):
+    with st.beta_expander("第2章 中高校生", expanded=False):
         imgpath = "07_timeline/Timeline.png"
         show_image(imgpath, "png")
-    with st.beta_expander("第3章 大学1回生", expanded=True):
+    with st.beta_expander("第3章 大学1回生", expanded=False):
         imgpath = "07_timeline/Timeline.png"
         show_image(imgpath, "png")
-    with st.beta_expander("第4章 大学2回生", expanded=True):
+    with st.beta_expander("第4章 大学2回生", expanded=False):
         imgpath = "07_timeline/Timeline.png"
         show_image(imgpath, "png")
-    with st.beta_expander("第5章 大学3回生", expanded=True):
+    with st.beta_expander("第5章 大学3回生", expanded=False):
         imgpath = "07_timeline/Timeline.png"
         show_image(imgpath, "png")
-    with st.beta_expander("第6章 大学4回生", expanded=True):
+    with st.beta_expander("第6章 大学4回生", expanded=False):
         imgpath = "07_timeline/Timeline.png"
         show_image(imgpath, "png")
-    with st.beta_expander("第7章 社会人", expanded=True):
+    with st.beta_expander("第7章 社会人", expanded=False):
         imgpath = "07_timeline/Timeline.png"
         show_image(imgpath, "png")
-    with st.beta_expander("番外 プロポーズ", expanded=True):
+    with st.beta_expander("番外編 EVE祭", expanded=False):
+        imgpath = "07_timeline/Timeline.png"
+        show_image(imgpath, "png")
+    with st.beta_expander("番外編 プロポーズ", expanded=False):
         imgpath = "07_timeline/Timeline.png"
         show_image(imgpath, "png")
     # with st.beta_expander("第8章 新婚生活", expanded=True):
