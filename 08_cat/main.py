6f572aa6743aeeafac3bca257113789e2b60591e
daikosh <daikoshg@gmail.com>
2021-07-11 09:35:06 +0900

    nyanko

--- 08_cat/main.py
+++ 08_cat/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image, ImageOps
 import os
+import datetime
 ## Parameters ##
 
 
@@ -8,10 +9,10 @@ import os
 def initialization():
     pass
 
-def show_image(imgpath, type):
+def show_image(imgpath):
     if os.path.exists(imgpath):
         image = Image.open(imgpath)
-        st.image(image, output_format=type, use_column_width="auto")
+        st.image(image, use_column_width="auto")
 
 def write_text(text, fontsize=18, align="left"):
     new_text = '<p style="font-family:sans-serif; text-align: {}; font-size: {}px;">{}</p>'.format(align, fontsize, text)
@@ -32,9 +33,21 @@ def main(debug):
         st.image(image, output_format="png", use_column_width="auto")
 
     ## Body ##
-    st.markdown("""
-        Coming soon...
-    """)
+    NOW_TIME = datetime.datetime.now() + datetime.timedelta(hours=9)
+    RELEASE_TIME = datetime.datetime(2022, 7, 10, 19, 00)
+    if RELEASE_TIME < NOW_TIME or debug is True: # リリース時間になったとき
+        st.write("投稿者: おみそ")
+        st.image("08_cat/img/01_cat.jpeg", caption="王者の風格")
+
+        st.write("投稿者: べ")
+        st.image("08_cat/img/02_cat.jpeg", output_format="jpeg", caption="だるまにされた猫です")
+
+
+    else: # リリース前
+        st.markdown("""
+            Coming soon...
+        """)
+
 
 
 if __name__ == "__main__":
