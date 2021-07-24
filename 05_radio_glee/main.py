38434ff3614dfb72bc6415ba2be20ca7f78355ad
daikosh <daikoshg@gmail.com>
2021-07-24 14:04:58 +0900

    glee

--- 05_radio_glee/main.py
+++ 05_radio_glee/main.py
@@ -20,8 +20,32 @@ def open_radio_expander(title, imgpath, mp3path):
 
 def main(debug):
     initialization()
-    ## Body ##
 
+    st.markdown("""
+        Still broader than our land of birth,
+
+        We've learned the oneness of our Earth;
+
+        Still higher than self-love we find
+
+        The love and service of mankind.
+
+        Dear Alma Mater, sons of thine
+
+        Would strive to live the life divine;
+
+        That we may with increasing years have stood
+
+        For Kyosuke, for Rikako, and Brotherhood!
+    """)
+    st.write("おたよりは[こちらのフォーム](https://forms.gle/eNde9TRwbGpRWnu49)で募集しています！")
+
+    imgpath = "line.png"
+    if os.path.exists(imgpath):
+        image = Image.open(imgpath)
+        st.image(image, output_format="png", use_column_width="auto")
+
+    ## Body ##
     open_radio_expander("#1 「恥ずかしながら帰ってまいりました。」 【出演: 阿久澤、松岡、林、松井】", '05_radio_glee/radio_1.jpg', '05_radio_glee/radio_1.m4a')
     st.write("To be continued ...")
     #open_radio_expander("#2 「珍苗字東京大決戦〜城之内死す〜」 【ゲスト: 阿久澤拓也、吉種伸彰】 (2021.6.12 収録)", '02_radio/02_radio_no2.jpg', '02_radio/02_radio_no2.mp3')
