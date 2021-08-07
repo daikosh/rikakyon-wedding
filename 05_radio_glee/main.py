54100df847b8b371d7adf9c2db91e189b64a09a3
daikosh <daikoshg@gmail.com>
2021-08-07 13:21:00 +0900

    timeline

--- 05_radio_glee/main.py
+++ 05_radio_glee/main.py
@@ -11,15 +11,14 @@ RELEASE_TIME = datetime.datetime(2021, 8, 7, 13, 00)
 def initialization():
     pass
 
-def open_radio_expander(title, imgpath, mp3path, format):
-    with st.beta_expander(title, expanded=True):
-        if os.path.exists(imgpath):
-            image = Image.open(imgpath)
-            st.image(image, use_column_width="auto")
-        if os.path.exists(mp3path):
-            audio_file = open(mp3path, 'rb')
-            audio_bytes = audio_file.read()
-            st.audio(audio_bytes, format='audio/{}'.format(format))
+def open_radio(imgpath, mp3path, format):
+    if os.path.exists(imgpath):
+        image = Image.open(imgpath)
+        st.image(image, use_column_width="auto")
+    if os.path.exists(mp3path):
+        audio_file = open(mp3path, 'rb')
+        audio_bytes = audio_file.read()
+        st.audio(audio_bytes, format='audio/{}'.format(format))
 
 def main(debug):
     initialization()
@@ -49,23 +48,18 @@ def main(debug):
         st.image(image, output_format="png", use_column_width="auto")
 
     ## Body ##
-    #if RELEASE_TIME < NOW_TIME or debug is True: # リリース時間になったとき
-    open_radio_expander("#1 「恥ずかしながら帰ってまいりました。」【出演: 阿久澤、松岡、林、松井】", '05_radio_glee/radio_1.jpg', '05_radio_glee/radio_1.m4a', 'm4a')
-    open_radio_expander("#2 「〈TL第1章〉楽器を始めてなかった頃。」【出演: 犬飼、松井、松岡、阿久澤】", '05_radio_glee/radio_2.png', '05_radio_glee/radio_2.mp3', 'mp3')
-    st.write("To be continued ...")
-    #else:
-        # open_radio_expander("#1 「恥ずかしながら帰ってまいりました。」【出演: 阿久澤、松岡、林、松井】", '05_radio_glee/radio_1.jpg', '05_radio_glee/radio_1.m4a', 'm4a')
-        # st.write("To be continued ...")
-
-
-    #open_radio_expander("#3 「」 【ゲスト: ？？？】 (2021.?.? 収録)", '02_radio/03_radio_no3.jpg', '02_radio/03_radio_no3.mp3')
-    #open_radio_expander("#4 「」 【ゲスト: ？？？】 (2021.?.? 収録)", '02_radio/04_radio_no4.jpg', '02_radio/04_radio_no4.mp3')
-    #open_radio_expander("#5 「」 【ゲスト: ？？？】 (2021.?.? 収録)", '02_radio/05_radio_no5.jpg', '02_radio/05_radio_no5.mp3')
-    #open_radio_expander("#6 「」 【ゲスト: ？？？】 (2021.?.? 収録)", '02_radio/06_radio_no6.jpg', '02_radio/06_radio_no6.mp3')
-    #open_radio_expander("#7 「」 【ゲスト: ？？？】 (2021.?.? 収録)", '02_radio/07_radio_no7.jpg', '02_radio/07_radio_no7.mp3')
-    #open_radio_expander("#8 「」 【ゲスト: ？？？】 (2021.?.? 収録)", '02_radio/08_radio_no8.jpg', '02_radio/08_radio_no8.mp3')
-    #open_radio_expander("#9 「」 【ゲスト: ？？？】 (2021.?.? 収録)", '02_radio/09_radio_no9.jpg', '02_radio/09_radio_no9.mp3')
-    #open_radio_expander("#10 「」 【ゲスト: ？？？】 (2021.?.? 収録)", '02_radio/10_radio_no10.jpg', '02_radio/10_radio_no10.mp3')
+    if RELEASE_TIME < NOW_TIME or debug is True: # リリース時間になったとき
+        with st.beta_expander('#1 「恥ずかしながら帰ってまいりました。」【出演: 阿久澤、松岡、林、松井】', expanded=False):
+            open_radio('05_radio_glee/radio_1.jpg', '05_radio_glee/radio_1.m4a', 'm4a')
+        with st.beta_expander('#2 「〈TL第1章〉楽器を始めてなかった頃。」【出演: 犬飼、松井、松岡、阿久澤】', expanded=True):
+            open_radio('05_radio_glee/radio_2.png', '05_radio_glee/radio_2.mp3', 'mp3')
+        st.write("To be continued ...")
+    else:
+        with st.beta_expander('#1 「恥ずかしながら帰ってまいりました。」【出演: 阿久澤、松岡、林、松井】', expanded=False):
+            open_radio('05_radio_glee/radio_1.jpg', '05_radio_glee/radio_1.m4a', 'm4a')
+        with st.beta_expander('#2 「〈TL第1章〉楽器を始めてなかった頃。」【出演: 犬飼、松井、松岡、阿久澤】', expanded=False):
+            open_radio('05_radio_glee/radio_2.png', '05_radio_glee/radio_2.mp3', 'mp3')
+        st.write("To be continued ...")
 
 
 if __name__ == "__main__":
