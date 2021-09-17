5924abea0131d9c328568b1c07823724d3715ac5
daikoshg@gmail.com <daikoshg@gmail.com>
2021-09-17 20:41:08 +0900

    radio3-3

--- 02_radio/main.py
+++ 02_radio/main.py
@@ -26,12 +26,14 @@ class Radio(object):
 
     def show_image(self, imgpath):
         """画像を表示"""
+
         if os.path.exists(imgpath):
             image = Image.open(imgpath)
             st.image(image, use_column_width="auto")
 
     def open_radio(self, mp3path):
         """ラジオを表示"""
+
         if os.path.exists(mp3path):
             audio_file = open(mp3path, 'rb')
             audio_bytes = audio_file.read()
@@ -39,12 +41,14 @@ class Radio(object):
 
     def open_radio_expander(self, title, imgpath, mp3path, expanded):
         """ラジオエクスパンダーを表示"""
+
         with st.beta_expander(title, expanded=expanded):
             self.show_image(imgpath)
             self.open_radio(mp3path)
 
     def write_text(self, text, fontsize, align):
         """テキストを表示"""
+
         new_text = '<p style="font-family:sans-serif; text-align: {}; font-size: {}px;">{}</p>'.format(align, fontsize, text)
         st.markdown(new_text, unsafe_allow_html=True)
 
@@ -63,24 +67,26 @@ class Radio(object):
         # ラジオ1回目を表示
         self.open_radio_expander(
             "#1 「人材紹介業Feeとりがち〜そうだ、顎削ろう〜」【ゲスト: 林寛人、松岡大起】 (2021.5.29 収録)",
-            '02_radio/01_radio_no1.jpg',
-            '02_radio/01_radio_no1.mp3',
+            '02_radio/01_radio_1.jpg',
+            '02_radio/01_radio_1.mp3',
             False
         )
+
         # ラジオ2回目を表示
         self.open_radio_expander(
             "#2 「珍名字東京大決戦〜城之内死す〜」【ゲスト: 阿久澤拓也、吉種伸彰】 (2021.6.12 収録)",
-            '02_radio/02_radio_no2.jpg',
-            '02_radio/02_radio_no2.mp3',
+            '02_radio/02_radio_2.jpg',
+            '02_radio/02_radio_2.mp3',
             False
         )
+
         # ラジオ3回目を表示
         with st.beta_expander(
             "#3 「聴取率を爆上げしたいので、弦のカリスマをゲストに呼びました。」【ゲスト: 和久あさぎ、毛利真夕】 (2021.7.22 収録)",
             expanded=True):
-            self.show_image("02_radio/03_radio_no3.jpg")
+            self.show_image("02_radio/03_radio_3.jpg")
             st.write("◯Part 1/3 「今野さん、毛利さん、お久しぶりです。」")
-            self.open_radio('02_radio/03_radio_no3.mp3')
+            self.open_radio('02_radio/03_radio_3-1.mp3')
             st.write("◯Part 2/3 「君はバレンティンを憶えているか？」")
             self.open_radio('02_radio/04_radio_3-2.mp3')
 
