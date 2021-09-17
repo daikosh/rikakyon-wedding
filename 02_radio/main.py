4c38d176427f3a9783336d133979a1241854bfdb
daikoshg@gmail.com <daikoshg@gmail.com>
2021-09-17 20:28:14 +0900

    radio3-3

--- 02_radio/main.py
+++ 02_radio/main.py
@@ -83,7 +83,7 @@ class Radio(object):
             self.open_radio('02_radio/03_radio_no3.mp3')
             st.write("◯Part 2/3 「君はバレンティンを憶えているか？」")
             self.open_radio('02_radio/04_radio_3-2.mp3')
-            st.write("{}".format(is_release(datetime.datetime(2021, 9, 17, 20, 25)))
+            st.write("{}".format(is_release(datetime.datetime(2021, 9, 17, 20, 25))))
             if is_release(datetime.datetime(2021, 9, 17, 20, 25)):
                 st.write("◯Part 3/3 「おじさんがハマってるものは大抵たのしい。」")
                 self.open_radio('02_radio/04_radio_3-3.mp3')
