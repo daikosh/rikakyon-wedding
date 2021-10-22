7c9c6647cb1889792851e6c4f82de9bef875a5a0
daikoshg@gmail.com <daikoshg@gmail.com>
2021-10-23 08:04:08 +0900

    radio4-3

--- 02_radio/main.py
+++ 02_radio/main.py
@@ -101,11 +101,10 @@ class Radio(object):
                 self.show_image("02_radio/04_radio_4.jpg")
                 st.write("◯Part 1/3 「ビューティフル・ネーム」")
                 self.open_radio('02_radio/04_radio_4-1.mp3')
-                if self.is_released(dt.datetime(2021, 10, 9, 13, 00)):
-                    st.write("◯Part 2/3 「ビューティフル・ロード」")
-                    self.open_radio('02_radio/04_radio_4-2.mp3')
-                    # st.write("◯Part 3/3 「ビューティフル・フード」")
-                    # self.open_radio('02_radio/04_radio_4-2.mp3')
+                st.write("◯Part 2/3 「ビューティフル・ロード」")
+                self.open_radio('02_radio/04_radio_4-2.mp3')
+                st.write("◯Part 3/3 「ビューティフル・フード」")
+                self.open_radio('02_radio/04_radio_4-3.mp3')
 
         # To be continued ... を表示
         self.write_text("To be continued ...", 16, "left")
